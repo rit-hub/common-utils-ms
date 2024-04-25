458b21f95d21a7007771c802ca1cb67a162704a3
rithub <ritamcharan232001@gmail.com>
2024-04-25 13:38:25 +0530

    initial commit

---
+++ src/main/java/com/rithub/commonutils/CommonUtilsMsApplication.java
@@ -0,0 +1,13 @@
+package com.rithub.commonutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonUtilsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonUtilsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rithub/commonutils/CommonUtilsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rithub.commonutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
